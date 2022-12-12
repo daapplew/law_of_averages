1b098edd6fac17ccd8f00d5992f67e056314b238
Dwayne Applewhite <dwayneaapplewhite@gmail.com>
2022-12-12 09:58:26 -0500

    fixed functionality for pitch and score

--- index.js
+++ index.js
@@ -2,18 +2,48 @@ const pitchBtn = document.getElementById('pitchBtn');
 const pitchNumber = document.getElementById('pitchNumber');
 const scoreBtn = document.getElementById('scoreBtn');
 const scoreNumber = document.getElementById('scoreNumber');
-const openModal = document.querySelector('#openModal')
+const bookedBtn = document.getElementById('bookedBtn');
+const bookedNumber = document.getElementById('bookedNumber');
+const modal = document.querySelector('#modal');
 const yesBtn = document.querySelector('#yesBtn');
+const noBtn = document.querySelector('#noBtn');
 
-let pitch = 0;
+let pitch = 1;
 function pitchIncrease(){
-    pitchNumber.innerHTML = pitch++
+    pitchNumber.innerHTML = pitch++;
 }
 pitchBtn.addEventListener('click',pitchIncrease);
 
+let score = 1
+function scoreIncrease(){
+    scoreNumber.innerHTML = score++;
+}
 scoreBtn.addEventListener('click',()=>{
-    openModal.showModal()
+    modal.showModal();
+    
 })
 
-yesBtn.addEventListener('click',pitchIncrease)
+bookedBtn.addEventListener('click',()=>{
+    modal.showModal();
+})
 
+
+
+function scoredPlusPitched(){
+    yesBtn.addEventListener('click',()=>{
+        pitchIncrease();
+        scoreIncrease();
+        modal.close();
+    })
+    
+}
+scoredPlusPitched()
+
+function scoredNoPitch(){
+    noBtn.addEventListener('click', ()=>{
+        pitchIncrease();
+        modal.close();
+    });
+    
+}
+scoredNoPitch()
