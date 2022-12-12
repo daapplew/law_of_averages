21d1c915c56e3a9956d31e60ba6fea7ea5390fb2
Dwayne Applewhite <dwayneaapplewhite@gmail.com>
2022-12-12 09:11:07 -0500

    added functionality to yesBtn when score is click

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const pitchBtn = document.getElementById('pitchBtn');
 const pitchNumber = document.getElementById('pitchNumber');
 const scoreBtn = document.getElementById('scoreBtn');
 const scoreNumber = document.getElementById('scoreNumber');
+const openModal = document.querySelector('#openModal')
+const yesBtn = document.querySelector('#yesBtn');
 
 let pitch = 0;
 function pitchIncrease(){
@@ -10,8 +12,8 @@ function pitchIncrease(){
 pitchBtn.addEventListener('click',pitchIncrease);
 
 scoreBtn.addEventListener('click',()=>{
-
+    openModal.showModal()
 })
 
-
+yesBtn.addEventListener('click',pitchIncrease)
 
